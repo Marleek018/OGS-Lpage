3c31fc5429c73c834d66353e35c0b8fba8f67ac6
Marleek018 <Abdulmarleek018@yahoo.com>
2024-03-14 07:50:50 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+
+    extend: {
+      colors: {
+        bla: '#161918',
+        gre: '#03BE5E',
+        deepblack:'#00160B',
+        Tblack: '#222624',
+        
+      },
+      fontFamily:{
+        Sora: ['Sora'],
+      }
+    },
+  },
+  plugins: [],
+}
+
