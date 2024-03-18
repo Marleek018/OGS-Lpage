8d327ba644c0afe368b388544441b7c76048afd9
Marleek018 <Abdulmarleek018@yahoo.com>
2024-03-18 05:00:38 -0700

    landing page update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,24 @@
 module.exports = {
   content: ["./public/*.html"],
   theme: {
+    screens: {
+      'xs':'320px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    
 
     extend: {
+      
       colors: {
         bla: '#161918',
         gre: '#03BE5E',
         deepblack:'#00160B',
         Tblack: '#222624',
+        Ablack:'#0F2010',
         
       },
       fontFamily:{
